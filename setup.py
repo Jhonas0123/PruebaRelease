bf273c69068ebfb8f5bae5860bdafea201af4575
Jhonatan Araujo <jhona01230123@gmail.com>
2025-07-07 22:18:50 -0400

    release de prueba realizado

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="pruebarelease",                           # Nombre del paquete
+    version="0.1.0",                                # Versión inicial
+    packages=find_packages(),                       # Paquetes a incluir
+    description="Un paquete pip simple de saludo",  # Breve descripción
+    author="Jhonatan Choque",                         # Tu nombre
+    author_email="jhona01230123@gmail.com",                 # Tu correo electrónico
+    url="https://github.com/jhonas0123/pruebarelease",     # URL del proyecto
+)
